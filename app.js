4ed18957c8bfdd00ee04081dcbac59ba063d2cf2
Coleman Grustas <grustas.c@northeastern.edu>
2025-12-13 14:32:36 -0500

    feat: setup express app boilerplate with basic index route
    - Install Express and ejs
    - Set up a basic index route
    - Create 'controllers', 'views', and 'errors' folders and files

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import indexRouter from "./routes/indexRouter.js";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Listening on port ${PORT}!`);
+});
